bc1a74f50056e17ffcd54cd5f44eae265e23e9ae
LimpingCoronation <as.voronin8@gmail.com>
2022-03-10 20:19:54 +0300

    .

---
+++ blog/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=250)
+    text = models.TextField()
+    pub_date = models.DateTimeField(auto_now=True)
